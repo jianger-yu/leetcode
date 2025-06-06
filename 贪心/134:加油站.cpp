3b24cad4d857219ba43a06ef298b21141a5ec747
jianger-yu <3299956263@qq.com>
2025-06-06 18:36:15 +0800

    6.6

--- 贪心/134:加油站.cpp
+++ 贪心/134:加油站.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool goaround(int st,int &end, vector<int>& gas, vector<int>& cost){
+    bool goaround(int st,int &end, vector<int>& gas, vector<int>& cost){
         int cnt = gas[st], i = st;
         while(1){
             cnt -= cost[i];
@@ -10,23 +10,23 @@ bool goaround(int st,int &end, vector<int>& gas, vector<int>& cost){
                 return false;
             }
             i++;
-            if(i == st) return true;
             if(i == gas.size()) i = 0;
+            if(i == st) return true;
             cnt = cnt + gas[i];
         }
     }
-int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-    int st = 0, end = 0, road = 0;
-    while(1){
-        if(goaround(st, end, gas, cost)) return st; 
-        else{
-            if(end > st)
-                road += end - st + 1;
-            else break;
-            st = end;
-            if(road >= gas.size()) break;
-            continue;
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int st = 0, end = 0, road = 0;
+        while(1){
+            if(goaround(st, end, gas, cost)) return st; 
+            else{
+                if(end > st)
+                    road = road + end - st;
+                else break;
+                st = end;
+                if(road >= gas.size()) break;
+                continue;
+            }
         }
+        return -1;
     }
-    return -1;
-}
