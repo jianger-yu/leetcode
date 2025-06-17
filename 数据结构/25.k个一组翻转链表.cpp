ea00408e49c0d469715c3b0846afb790f12073af
jianger-yu <3299956263@qq.com>
2025-06-17 19:11:50 +0800

    6.15

---
+++ 数据结构/25.k个一组翻转链表.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head->next == NULL) return head;
+        ListNode* arr[5010];
+        int size = -1;
+        while(head != NULL){
+            arr[++size] = head;
+            head = head -> next;
+        }
+        int i = 0, t = size / k;
+        for(i = 0; i < t; i++){
+            int st = i * k;
+            for(int j = st; j < st + k; j++){
+                if(j == st) arr[j]->next = arr[st+k-1]->next;
+                else arr[j]->next = arr[j - 1];
+                if(j == st+k-1){
+                    if(i == 0)
+                        head = arr[j];
+                    if((i+2)*k - 1 <= size)
+                        arr[st]->next = arr[(i+2)*k - 1];
+                }
+            }
+        }
+        return head;
+    }
+};
